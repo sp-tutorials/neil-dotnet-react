36967010fa17a788a8ea9f30c9b663ae77812de1
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-01-27 22:03:36 +0200

    2.3. Reviewing the project files and startup
    add required assets to build and debug

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,7 +16,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
 
 var summaries = new[]
 {
