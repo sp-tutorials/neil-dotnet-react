a2e35d568815563fb065fb1e5a0d3d64b94938a5
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-01-29 22:46:35 +0200

    Revert "4.12. Cancellation tokens"
    This reverts commit 421875a7662c8073cedd6bb72a667f302508bbb2.

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,7 +1,6 @@
 using Domain;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using Persistence;
 
 namespace Application.Activities
@@ -13,29 +12,14 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-            public ILogger<List> _logger { get; }
-            public Handler(DataContext context, ILogger<List> logger)
+            public Handler(DataContext context)
             {
-                _logger = logger;
                 _context = context;
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                try
-                {
-                    for (int i = 0; i < 10; i++)
-                    {
-                        cancellationToken.ThrowIfCancellationRequested();
-                        await Task.Delay(1000, cancellationToken);
-                        _logger.LogInformation($"Task {i} has completed");
-                    }
-                }
-                catch (Exception ex) when (ex is TaskCanceledException)
-                {
-                    _logger.LogInformation($"Task was cancelled");
-                }
-                return await _context.Activities.ToListAsync(cancellationToken);
+                return await _context.Activities.ToListAsync();
             }
         }
     }
