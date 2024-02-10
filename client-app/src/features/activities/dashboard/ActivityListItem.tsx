0930425aa1c7ec1100a10de90c4bba9b36f39692
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-02-10 23:01:48 +0200

    9.8. Styling the home page

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,24 +1,13 @@
-import React, { SyntheticEvent, useState } from 'react';
-import { Button, Icon, Item, Label, Segment } from 'semantic-ui-react';
+import React from 'react';
+import { Button, Icon, Item, Segment } from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import { Link } from 'react-router-dom';
-import { useStore } from '../../../app/stores/store';
 
 interface Props {
     activity: Activity;
 }
 
 export default function ActivityListItem({ activity }: Props) {
-    const { activityStore } = useStore();
-    const { deleteActivity, loading } = activityStore;
-
-    const [target, setTarget] = useState('');
-
-    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-        setTarget(e.currentTarget.name);
-        deleteActivity(id);
-    }
-
     return (
         <Segment.Group>
             <Segment>
