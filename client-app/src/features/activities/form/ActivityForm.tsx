7fab676e131d997f3e3c87d21af38e287c4d852f
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-02-21 10:39:18 +0200

    11.7. Creating a reusable select input

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -10,6 +10,8 @@ import { Formik, Form, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import MyTextInput from '../../../app/common/form/MyTextInput';
 import MyTextArea from '../../../app/common/form/MyTextArea';
+import { categoryOptions } from '../../../app/common/form/options/categoryOptions';
+import MySelectInput from '../../../app/common/form/MySelectInput';
 
 export default observer(function ActivityForm() {
     const history = useHistory();
@@ -71,7 +73,7 @@ export default observer(function ActivityForm() {
                     <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
                         <MyTextInput name='title' placeholder='Title' />
                         <MyTextArea rows={3} placeholder='Description' name='description' />
-                        <MyTextInput placeholder='Category' name='category' />
+                        <MySelectInput options={categoryOptions} placeholder='Category' name='category' />
                         <MyTextInput placeholder='Date' name='date' />
                         <MyTextInput placeholder='City' name='city' />
                         <MyTextInput placeholder='Venue' name='venue' />
