25cb5ab54ce922146d293b9281c6e27993de92af
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-05 13:24:22 +0200

    13.12. Handling validation errors in the register form

--- client-app/src/features/users/RegisterForm.tsx
+++ client-app/src/features/users/RegisterForm.tsx
@@ -1,10 +1,11 @@
 import { ErrorMessage, Form, Formik } from 'formik';
 import React from 'react';
 import MyTextInput from '../../app/common/form/MyTextInput';
-import { Button, Header, Label } from 'semantic-ui-react';
+import { Button, Header } from 'semantic-ui-react';
 import { useStore } from '../../app/stores/store';
 import { observer } from 'mobx-react-lite';
 import * as Yup from 'yup';
+import ValidationErrors from '../errors/ValidationErrors';
 
 
 export default observer(function RegisterForm() {
@@ -13,7 +14,7 @@ export default observer(function RegisterForm() {
         <Formik
             initialValues={{ displayName: '', username: '', email: '', password: '', error: null }}
             onSubmit={(values, { setErrors }) => userStore.register(values).catch(error =>
-                setErrors({ error: 'Invalid email or password' }))}
+                setErrors({ error }))}
             validationSchema={Yup.object({
                 displayName: Yup.string().required(),
                 username: Yup.string().required(),
@@ -22,7 +23,7 @@ export default observer(function RegisterForm() {
             })}
         >
             {({ handleSubmit, isSubmitting, errors, isValid, dirty }) => (
-                <Form className='ui form' onSubmit={handleSubmit} autoComplete='off'>
+                <Form className='ui form error' onSubmit={handleSubmit} autoComplete='off'>
                     <Header as='h2' content='Sign up to Reactivities' color='teal' textAlign='center' />
                     <MyTextInput name='displayName' placeholder='DisplayName' />
                     <MyTextInput name='username' placeholder='Username' />
@@ -30,7 +31,7 @@ export default observer(function RegisterForm() {
                     <MyTextInput name='password' placeholder='Password' type='password' />
                     <ErrorMessage
                         name='error' render={() =>
-                            <Label style={{ marginBottom: 10 }} basic color='red' content={errors.error} />}
+                            <ValidationErrors errors={errors.error} />}
                     />
                     <Button disabled={!isValid || !dirty || isSubmitting}
                         loading={isSubmitting} positive content='Register' type='submit' fluid />
