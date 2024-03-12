9cde8b18489bad6d183e0439a1623530e72c5e16
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-12 19:11:08 +0200

    20.10. Adding methods to follow and unfollow

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -93,7 +93,8 @@ const Profiles = {
     },
     setMainPhoto: (id: string) => requests.post(`/photos/${id}/setMain`, {}),
     deletePhoto: (id: string) => requests.del(`/photos/${id}`),
-    update: (profile: Partial<Profile>) => requests.put(`/profiles`, profile)
+    update: (profile: Partial<Profile>) => requests.put(`/profiles`, profile),
+    updateFollowing: (username: string) => requests.post(`/follow/${username}`, {})
 }
 
 const agent = {
