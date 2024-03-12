dfa0a2c49e556d6bb2d85cc34e7bad1acd22fd7d
Patrulescu-Ronald-Sandrino <prie2985@scs.ubbcluj.ro>
2024-03-12 09:40:21 +0200

    19.10. Sending comments

--- client-app/src/features/activities/details/ActivityDetailedChat.tsx
+++ client-app/src/features/activities/details/ActivityDetailedChat.tsx
@@ -1,8 +1,10 @@
 import { observer } from 'mobx-react-lite'
 import React, { useEffect } from 'react'
-import { Segment, Header, Comment, Form, Button } from 'semantic-ui-react'
+import { Segment, Header, Comment, Button } from 'semantic-ui-react'
 import { useStore } from '../../../app/stores/store';
 import { Link } from 'react-router-dom';
+import { Formik, Form } from 'formik';
+import MyTextArea from '../../../app/common/form/MyTextArea';
 
 interface Props {
     activityId: string;
@@ -31,7 +33,7 @@ export default observer(function ActivityDetailedChat({ activityId }: Props) {
             >
                 <Header>Chat about this event</Header>
             </Segment>
-            <Segment attached>
+            <Segment attached clearing>
                 <Comment.Group>
                     {commentStore.comments.map(comment => (
                         <Comment key={comment.id}>
@@ -41,22 +43,34 @@ export default observer(function ActivityDetailedChat({ activityId }: Props) {
                                     {comment.displayName}
                                 </Comment.Author>
                                 <Comment.Metadata>
-                                    <div>{comment.createdAt.toISOString()}</div>
+                                    <div>{comment.createdAt as any}</div>
                                 </Comment.Metadata>
                                 <Comment.Text>{comment.body}</Comment.Text>
                             </Comment.Content>
                         </Comment>
                     ))}
 
-                    <Form reply>
-                        <Form.TextArea />
-                        <Button
-                            content='Add Reply'
-                            labelPosition='left'
-                            icon='edit'
-                            primary
-                        />
-                    </Form>
+                    <Formik
+                        onSubmit={(values, { resetForm }) => commentStore.addComment(values).then(() => resetForm())}
+                        initialValues={{ body: '' }}
+                    >
+                        {({ isSubmitting, isValid }) => (
+                            <Form className='ui form'>
+                                <MyTextArea placeholder='Add comment' name='body' rows={2} />
+                                <Button
+                                    loading={isSubmitting}
+                                    disabled={isSubmitting || !isValid}
+                                    content='Add Reply'
+                                    labelPosition='left'
+                                    icon='edit'
+                                    primary
+                                    type='submit'
+                                    floated='right'
+                                />
+                            </Form>
+                        )}
+                    </Formik>
+
                 </Comment.Group>
             </Segment>
         </>
